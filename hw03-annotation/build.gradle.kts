45817e98f6ee3e8a02c2c280f4610d1fa0c8febc
chvals <chvals@gmail.com>
2024-12-03 23:27:28 +0300

    start commit

---
+++ hw03-annotation/build.gradle.kts
@@ -0,0 +1,30 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id ("com.github.johnrengelman.shadow")
+}
+
+dependencies {
+    implementation ("ch.qos.logback:logback-classic")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("annotationsHW02")
+        archiveVersion.set("1.0")
+        archiveClassifier.set("")
+        manifest {
+            attributes(mapOf("Main-Class" to "Main"))
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ hw03-annotation/src/main/java/ru/otus/TestFrameworkDemo.java
@@ -0,0 +1,7 @@
+package ru.otus;
+
+public class TestFrameworkDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
