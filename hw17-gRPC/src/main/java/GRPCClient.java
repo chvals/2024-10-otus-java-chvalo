e5420c089d7be7a481fe21d67a7b12f064b690d1
chvals <chvals@gmail.com>
2025-04-14 00:43:42 +0300

    hw17 2 commit add try finally

--- hw17-gRPC/src/main/java/GRPCClient.java
+++ hw17-gRPC/src/main/java/GRPCClient.java
@@ -23,47 +23,50 @@ public class GRPCClient {
                 .usePlaintext()
                 .build();
 
-        var newStub = RemoteNumberGenerateServiceGrpc.newStub(channel);
-        StartMessage startMessage = StartMessage.newBuilder()
-                .setFirstValue(FIRST_VALUE)
-                .setLastValue(LAST_VALUE)
-                .build();
-        Queue<Long> dataBuffer = new ConcurrentLinkedQueue<>();
-        newStub.generateNumber(startMessage, new StreamObserver<ResponseMessage>() {
-            @Override
-            public void onNext(ResponseMessage responseMessage) {
-                long value = responseMessage.getValue();
-                log.info("New value from server: {}", value);
-                dataBuffer.offer(value);
-            }
+        try {
+            var newStub = RemoteNumberGenerateServiceGrpc.newStub(channel);
+            StartMessage startMessage = StartMessage.newBuilder()
+                    .setFirstValue(FIRST_VALUE)
+                    .setLastValue(LAST_VALUE)
+                    .build();
+            Queue<Long> dataBuffer = new ConcurrentLinkedQueue<>();
+            newStub.generateNumber(startMessage, new StreamObserver<ResponseMessage>() {
+                @Override
+                public void onNext(ResponseMessage responseMessage) {
+                    long value = responseMessage.getValue();
+                    log.info("New value from server: {}", value);
+                    dataBuffer.offer(value);
+                }
 
-            @Override
-            public void onError(Throwable t) {
-                log.error(t.getMessage());
-            }
+                @Override
+                public void onError(Throwable t) {
+                    log.error(t.getMessage());
+                }
 
-            @Override
-            public void onCompleted() {
-                log.info("Completed!");
-            }
-        });
+                @Override
+                public void onCompleted() {
+                    log.info("Completed!");
+                }
+            });
 
-        long currentValue = 0;
-        for (int i = 0; i <= MAX_CLIENT_ITERATION; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            long currentValue = 0;
+            for (int i = 0; i <= MAX_CLIENT_ITERATION; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                long lastValueFromServer = 0;
+                Long valueFromServer;
+                while ((valueFromServer = dataBuffer.poll()) != null) {
+                    lastValueFromServer = valueFromServer;
+                }
+                currentValue = currentValue + lastValueFromServer + 1;
+                log.info("Current value: {}", currentValue);
             }
-            long lastValueFromServer = 0;
-            Long valueFromServer;
-            while ((valueFromServer = dataBuffer.poll()) != null) {
-                lastValueFromServer = valueFromServer;
-            }
-            currentValue = currentValue + lastValueFromServer + 1;
-            log.info("Current value: {}", currentValue);
+            
+        } finally {
+            channel.shutdown();
         }
-
-        channel.shutdown();
     }
 }
