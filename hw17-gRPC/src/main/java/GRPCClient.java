c179a20ecbcf80c34168a0ad95ba38defe004f0f
chvals <chvals@gmail.com>
2025-04-16 22:55:38 +0300

    hw17 использование переменной вместо очереди

--- hw17-gRPC/src/main/java/GRPCClient.java
+++ hw17-gRPC/src/main/java/GRPCClient.java
@@ -1,7 +1,5 @@
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.protobuf.RemoteNumberGenerateServiceGrpc;
@@ -18,6 +16,8 @@ public class GRPCClient {
     private static final int LAST_VALUE = 30;
     private static final int MAX_CLIENT_ITERATION = 50;
 
+    private static volatile long currentValueFromServer = 0;
+
     public static void main(String[] args) throws InterruptedException {
         var channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
                 .usePlaintext()
@@ -29,13 +29,12 @@ public class GRPCClient {
                     .setFirstValue(FIRST_VALUE)
                     .setLastValue(LAST_VALUE)
                     .build();
-            Queue<Long> dataBuffer = new ConcurrentLinkedQueue<>();
+
             newStub.generateNumber(startMessage, new StreamObserver<ResponseMessage>() {
                 @Override
                 public void onNext(ResponseMessage responseMessage) {
-                    long value = responseMessage.getValue();
-                    log.info("New value from server: {}", value);
-                    dataBuffer.offer(value);
+                    currentValueFromServer = responseMessage.getValue();
+                    log.info("New value from server: {}", currentValueFromServer);
                 }
 
                 @Override
@@ -50,21 +49,19 @@ public class GRPCClient {
             });
 
             long currentValue = 0;
+            long oldValueFromServer = 0;
             for (int i = 0; i <= MAX_CLIENT_ITERATION; i++) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                long lastValueFromServer = 0;
-                Long valueFromServer;
-                while ((valueFromServer = dataBuffer.poll()) != null) {
-                    lastValueFromServer = valueFromServer;
-                }
+                long lastValueFromServer = currentValueFromServer != oldValueFromServer ? currentValueFromServer : 0;
                 currentValue = currentValue + lastValueFromServer + 1;
                 log.info("Current value: {}", currentValue);
+                oldValueFromServer = lastValueFromServer;
             }
-            
+
         } finally {
             channel.shutdown();
         }
